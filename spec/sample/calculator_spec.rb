59805210aef8cc2b3b50de3e7cbfce3597e737b0
John-Kimani <kimwaweru100@gmail.com>
2022-10-30 21:50:00 +0300

    Test: create first test install minitest and Rspec

---
+++ spec/sample/calculator_spec.rb
@@ -0,0 +1,13 @@
+def sum(num1, num2)
+    num1 + num2
+end
+
+describe 'CALCULTOR' do
+
+    context 'when 1 adds 2' do
+        it 'succeeds' do 
+            add = sum(1, 2)
+            expect(add).to equal(3)
+        end
+    end
+end 
